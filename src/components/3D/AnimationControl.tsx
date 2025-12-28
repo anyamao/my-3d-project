347460e78d1204609dae95336376c1f604e10495
anya <vikashinder@gmail.com>
2025-12-28 14:14:32 +0700

    I changed the font and removed the shadows as they were causing performance issues. Sad!

--- src/components/3D/AnimationControl.tsx
+++ src/components/3D/AnimationControl.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Search, Sun, Wind, CloudRain, Droplet } from "lucide-react";
+import { Search, Sun, Wind, CloudRain, Droplet, Settings } from "lucide-react";
 
 interface AnimationControlsProps {
   onPlayToFrame50: () => void;
@@ -23,11 +23,12 @@ const AnimationControls: React.FC<AnimationControlsProps> = ({
   return (
     <div className="relative top-0 mt-[-30px]  ml-[20px] flex  z-10 ">
       <a
-        className="text-[14px] fixed text-white z-30 top-0 right-0 mt-[20px] mr-[40px]"
+        className="text-[16px] fixed  text-white z-30 top-0 right-0 mt-[20px] mr-[50px] cursor-pointer hover:underline"
         href="https://t.me/anyamaoo"
       >
         anyamao's weather app
       </a>
+      <Settings className="text-lime-600 w-[25px] h-[25px] fixed top-0 right-0 mt-[20px] hover:scale-125 transition-translate duration-300 transition-all hover:rotate-45 cursor-pointer mr-[10px]"></Settings>
 
       <img
         alt="Dashboard"
@@ -40,7 +41,7 @@ const AnimationControls: React.FC<AnimationControlsProps> = ({
           <input
             type="text"
             placeholder="Type your city here..."
-            className="outline-none bg-transparent text-[13px] text-gray-600"
+            className="outline-none bg-transparent text-[15px] text-gray-600"
           ></input>
         </div>
         <div className="w-full flex  mt-[15px] justify-between border-b-[1px] border-b-gray-300  p-[10px] pb-[20px]">
@@ -48,23 +49,23 @@ const AnimationControls: React.FC<AnimationControlsProps> = ({
             <div className="text-[30px]">London</div>
 
             <div className="text-gray-600 text-[30px] mt-[-10px]"> 15°C</div>
-            <div className="text-gray-500 text-[15px]">Sunny</div>
+            <div className="text-gray-500 text-[16px]">Sunny</div>
           </div>
           <Sun className="text-yellow-400 w-[100px] h-[100px] mr-[10px]"></Sun>
         </div>
         <div className="flex flex-row justify-between mt-[25px] px-[5px]  pb-[30px]  border-b-[1px] border-b-gray-300 ">
           <div className="flex flex-col items-start">
-            <div className="text-[14px] text-gray-500">Wind Speed</div>
+            <div className="text-[16px] text-gray-500">Wind Speed</div>
             <Wind className="w-[40px] h-[40px] text-lime-600 "></Wind>
             <div className="text-[20px]">12 m/s</div>
           </div>
           <div className="flex flex-col items-center">
-            <div className="text-[14px] text-gray-500">Humidity</div>
+            <div className="text-[16px] text-gray-500">Humidity</div>
             <Droplet className="w-[40px] h-[40px] text-lime-600 "></Droplet>
             <div className="text-[20px]">40%</div>
           </div>
           <div className="flex flex-col items-end">
-            <div className="text-[14px] text-gray-500">Rain Chance</div>
+            <div className="text-[16px] text-gray-500">Rain Chance</div>
             <CloudRain className="w-[40px] h-[40px] text-lime-600 "></CloudRain>
             <div className="text-[20px]">30%</div>
           </div>
--- src/components/3D/Scene.tsx
+++ src/components/3D/Scene.tsx
@@ -73,14 +73,6 @@ const Scene: React.FC = () => {
           position={[20, 30, 10]}
           intensity={5}
           color="#ffffffff"
-          castShadow
-          shadow-mapSize={[8192, 8192]}
-          shadow-camera-left={-50}
-          shadow-camera-right={50}
-          shadow-camera-top={50}
-          shadow-camera-bottom={-50}
-          shadow-camera-far={200}
-          shadow-bias={-0.001}
         />
         <directionalLight
           position={[10, 5, 0]}
